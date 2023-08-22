57ffb9652a956697c262cd77329a9595d3b6c4c5
AnshumanPandey001 <140974241+AnshumanPandey001@users.noreply.github.com>
2023-08-22 21:19:51 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,28 @@
+var redirectButton = document.getElementById("link1");
+redirectButton.onclick = function () {
+    window.location.href = "https://github.com/AnshumanPandey001";
+};
+
+var redirectButton = document.getElementById("link2");
+
+
+redirectButton.onclick = function () {
+    window.location.href = "https://www.linkedin.com/in/anshuman-pandey-bb6ab5243/";
+};
+
+var redirectButton = document.getElementById("link3");
+redirectButton.onclick = function () {
+    window.location.href = "mailto:exploreanshu001@gmail.com";
+};
+
+var redirectButton = document.getElementById("link4");
+
+redirectButton.onclick = function () {
+    window.location.href = "https://twitter.com/exploreanshu001?t=tUxuJXWfTzLmH5N7fqrikg&s=09";
+};
+
+var redirectButton = document.getElementById("link5");
+
+redirectButton.onclick = function () {
+    window.location.href = "https://instagram.com/exploreanshu001?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D";
+};
---
+++ protfolio.js
@@ -0,0 +1,25 @@
+var redirectButton = document.getElementById("link1");
+redirectButton.onclick = function () {
+    window.location.href = "https://github.com/AnshumanPandey001";
+};
+
+var redirectButton = document.getElementById("link2");
+
+
+redirectButton.onclick = function () {
+    window.location.href = "https://www.linkedin.com/in/anshuman-pandey-bb6ab5243/";
+};
+
+var redirectButton = document.getElementById("link3");
+redirectButton.onclick = function () {
+    window.location.href = "mailto:exploreanshu001@gmail.com";
+};
+
+var redirectButton = document.getElementById("link4");
+
+redirectButton.onclick = function () {
+    window.location.href = "https://drive.google.com/file/d/11m7xplb0F3762HSgGbagSWavijFE2VqX/view?usp=drive_link";
+};
+
+
+
